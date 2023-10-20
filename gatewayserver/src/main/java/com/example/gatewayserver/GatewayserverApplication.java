0ddbdfc651fc09b55272de6259becea0ac9aea30
shantanu <shantanu.barua@vivacomsolutions.com>
2023-10-20 16:49:59 +0200

    Implements custom routing

--- gatewayserver/src/main/java/com/example/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/example/gatewayserver/GatewayserverApplication.java
@@ -2,12 +2,24 @@ package com.example.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayserverApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayserverApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayserverApplication.class, args);
+    }
+        @Bean
+        public RouteLocator bankRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+            return routeLocatorBuilder.routes()
+                    .route(p-> p.path("/bank/accounts/**")
+                            .filters(w-> w.rewritePath("/bank/accounts/(?<segment>.*)","/$(segment)"))
+                            .uri("lb:ACCOUNTS")).build();
+        }
+
+
 
 }
